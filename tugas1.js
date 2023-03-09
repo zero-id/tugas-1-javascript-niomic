45d69aa411f062e14754c9704f0d956b38813f40
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-09 14:22:56 +0700

    tugas 1 js

---
+++ tugas1.js
@@ -0,0 +1,8 @@
+var firstName = "Abdul";
+let lastName = "Latif Faturahman";
+let nickName = "Latif";
+const age = "22 tahun";
+console.log(firstName);
+console.log(lastName);
+console.log(nickName);
+console.log(age);
